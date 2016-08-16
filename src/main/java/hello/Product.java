0ea021c993f255706ed9d069efa366e2a0c66f07
Matt Mowris <matt.mowris@gmail.com>
2016-08-16 15:28:24 -0700

    test

---
+++ src/main/java/hello/Product.java
@@ -0,0 +1,20 @@
+package hello;
+
+public class Product {
+
+    private final long id;
+    private final String content;
+
+    public Product(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
---
+++ src/main/java/hello/ProductController.java
@@ -0,0 +1,41 @@
+package hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.*;
+
+@RestController
+public class GreetingController {
+
+	Connection connection = getConnection();
+	Statement stmt = connection.createStatement();
+	ResultSet rs = stmt.executeQuery("SELECT * FROM salesforce.product2");
+
+	while (rs.next()) {
+		System.out.println("Read from DB: " + rs.getString(3));
+		private static final String template = rs.getString(3));
+	}
+
+
+	private final AtomicLong counter = new AtomicLong();
+
+	@RequestMapping("/index")
+	public Product product(@RequestParam(value="name", defaultValue="World") String name) {
+		return new Product(counter.incrementAndGet(),
+			String.format(template, name));
+	}
+
+	private static Connection getConnection() throws URISyntaxException, SQLException {
+		URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+		String username = dbUri.getUserInfo().split(":")[0];
+		String password = dbUri.getUserInfo().split(":")[1];
+		String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
+
+		return DriverManager.getConnection(dbUrl, username, password);
+	}
+}
