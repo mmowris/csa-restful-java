dd8b98c95703067e84081bfb4b2d3812461660cf
Matt Mowris <matt.mowris@gmail.com>
2016-08-17 14:26:56 -0700

    get working

--- src/main/java/hello/Product.java
+++ src/main/java/hello/Product.java
@@ -3,18 +3,18 @@ package hello;
 public class Product {
 
     private final String name;
-    private final String id;
+    private final String productcode;
 
-    public Product(String name, String id) {
+    public Product(String name, String productcode) {
         this.name = name;
-        this.id = id;
+        this.productcode = productcode;
     }
 
     public String getName() {
         return name;
     }
 
-    public String getId() {
-        return id;
+    public String getProductcode() {
+        return productcode;
     }
 }
--- src/main/java/hello/ProductController.java
+++ src/main/java/hello/ProductController.java
@@ -4,8 +4,6 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
-import static org.springframework.web.bind.annotation.RequestMethod.POST;
 import java.sql.*;
 import java.util.HashMap;
 import java.util.ArrayList;
@@ -22,13 +20,9 @@ public class ProductController {
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
-	
-
-	
-
-
-	@RequestMapping(value="/index", method=RequestMethod.GET)
-	public ArrayList<Product> func() {
+	/* GET Function */
+	@RequestMapping("/index")
+	public ArrayList<Product> listProducts() {
 
 		try {
 			ArrayList<Product> output = new ArrayList<Product>();
@@ -36,6 +30,35 @@ public class ProductController {
 
 			connection = DatabaseUrl.extract().getConnection();
 			Statement stmt = connection.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM salesforce.product2");
+
+			while (rs.next()) {
+				Product p = new Product(rs.getString(4), rs.getString(10));
+				output.add(p);
+
+			}
+
+			return output;
+		} catch(Exception e) {
+
+		}
+
+		return null;
+		
+	}
+
+	@RequestMapping("/new")
+	public ArrayList<Product> listProducts() {
+
+		try {
+			ArrayList<Product> output = new ArrayList<Product>();
+			Connection connection = null;
+
+			connection = DatabaseUrl.extract().getConnection();
+			Statement stmt = connection.createStatement();
+			String name = @RequestParam(value="name", defaultValue="NewProduct");
+			String productcode = @RequestParam(value="pc", defaultValue="0000");
+			Statement.executeUpdate("INSERT INTO salesforce.product2 (name, productcode) VALUES ('" + name +"','" +productcode +"');");
 			ResultSet rs = stmt.executeQuery("SELECT * FROM salesforce.product2");
 
 			while (rs.next()) {
